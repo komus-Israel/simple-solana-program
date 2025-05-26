bcca960c164536401758a39aaa8616f9a8dad6bd
komus-Israel <komolehinisrael@gmail.com>
2025-05-26 19:50:47 +0100

    Define Mint data

--- programs/spl/src/lib.rs
+++ programs/spl/src/lib.rs
@@ -1,9 +1,11 @@
-use anchor_lang::prelude::*;
+use anchor_lang::{prelude::*, solana_program::program_option::COption};
+
+
 
 declare_id!("21HrvicsFGysih8H4CfUEM41KG6cpZUqqvUz4eiikydC");
 
 #[program]
-pub mod spl {
+pub mod zeno {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
@@ -11,6 +13,29 @@ pub mod spl {
         Ok(())
     }
 }
+
+/// MINT ACCOUNT
+/// A mint acccount is an account type in Solana's Token program 
+/// that uniquely represents a token on the network
+/// and stores global metadata about the token, such as its total supply, decimals, and other attributes.
+
+/// Mint data
+#[repr(C)]
+#[derive(Clone, Copy, Debug, Default, PartialEq)]
+pub struct Mint {
+    /// optional authority used to mint new tokens
+    pub mint_authority: COption<Pubkey>,
+    /// total supply
+    pub supply: u64,
+    /// number of base 10 digits to the right of the decimal place
+    pub decimals: u8,
+    /// is `true` if this struct has been initialized
+    pub is_initialized: bool,
+    /// optional authority that can freeze token accounts
+    pub freeze_authority: COption<Pubkey>,
+}
+
+
 
 #[derive(Accounts)]
 pub struct Initialize {}
